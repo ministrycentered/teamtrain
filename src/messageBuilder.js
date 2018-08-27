02e6ffd23e493f75ad52f70771b6e5004d18ddd5
Daniel Ma <drailskid@yahoo.com>
2018-08-27 13:25:25 -0700

    A step in the right direction: move google sheets to one file

--- src/messageBuilder.js
+++ src/messageBuilder.js
@@ -1,5 +1,6 @@
 const redisClient = require("./redisClient")
 const slackClient = require("./slackClient")
+const googleSheetsClient = require("./googleSheetsClient")
 const DEFAULT_ATTACHMENT = {
   pretext:
     "Time to board the train! Train departs NOW. You've got 15 minutes to converse with each other.",
@@ -44,34 +45,10 @@ exports.buildAttachments = async function buildAttachments(payload, members = nu
     }
   }
 
-  let personalprompt = await getPersonalPrompt()
-  let workprompt = await getWorkPrompt()
+  const personalprompt = await googleSheetsClient.getPersonalPrompt()
+  const workprompt = await googleSheetsClient.getWorkPrompt()
   promptAttachment.fields[0]["value"] = personalprompt
   promptAttachment.fields[1]["value"] = workprompt
 
   return [promptAttachment, whoCallsAttachment]
-}
-
-const { promisify } = require("util")
-const { google } = require("googleapis")
-const googleConfig = require("config").get("google")
-const oauth2Client = new google.auth.OAuth2(googleConfig.clientId, googleConfig.clientSecret, "")
-oauth2Client.setCredentials({ refresh_token: googleConfig.refreshToken })
-const sheets = google.sheets({ version: "v4", auth: oauth2Client })
-const valuesGetAsync = promisify(sheets.spreadsheets.values.get)
-
-async function getPersonalPrompt() {
-  const {
-    data: { values }
-  } = await valuesGetAsync({ spreadsheetId: googleConfig.sheetId, range: "A2:A" })
-
-  return values[Math.floor(Math.random() * values.length)]
-}
-
-async function getWorkPrompt() {
-  const {
-    data: { values }
-  } = await valuesGetAsync({ spreadsheetId: googleConfig.sheetId, range: "B2:B" })
-
-  return values[Math.floor(Math.random() * values.length)]
 }
